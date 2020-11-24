ee3195e5a8e3ff84ec63cf1e052dc578d15e0138
awurama <mimidede222@gmail.com>
2020-11-24 09:21:59 -0800

    react-sept

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+
+function App() {
+  return (
+    <div style={{ backgroundColor: 'pink', width: '100vw', height: '100vh', }}>
+       <div style={{width: '20vw', height: '20vh', border: '1px solid black'}}>
+         <h4>Box 1</h4>
+       </div>
+       
+       <div style={{width: '20vw', height: '20vh', border: '1px solid black'}}>
+         <h4>Box 2</h4>
+       </div>
+
+       <div style={{width: '20vw', height: '20vh', border: '1px solid black'}}>
+         <h4>Box 3</h4>
+       </div>
+    </div>
+  );
+}
+
+export default App;
